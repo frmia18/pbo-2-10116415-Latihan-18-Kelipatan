e16d53d827128f2bfe77318197277bbc2b4e3cf3
frmia <frmiafm@gmail.com>
2020-10-30 21:58:09 +0700

    first commit

---
+++ src/kelipatann/Kelipatann.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatann;
+
+/**
+ *
+ * @author Frda
+ * KELAS : PBO 2
+ * NIM : 10116415
+ * Deskripsi program: program ini berisi program untuk menampilkan hasil
+ * kelipatan angka 3,5 secara menaik
+ 
+ */
+public class Kelipatann {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     for (double i = 3.5; i <= 35; i += 3.5){
+         System.out.println(i);
+     }
+    }
+}
